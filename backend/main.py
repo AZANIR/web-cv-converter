54b4d26b2c95b2efff50096782e573aeee05e53b
AZANIR <eleoneks@gmail.com>
2026-04-11 10:36:58 +0300

    fix(backend): restrict CORS allow_methods and allow_headers

--- backend/main.py
+++ backend/main.py
@@ -29,8 +29,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=_origins or ["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type", "X-Auth0-ID-Token"],
 )
 
 
